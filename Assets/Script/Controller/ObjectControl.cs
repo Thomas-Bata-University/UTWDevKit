d72489644a39d328eb512761ba07591ebfc2b82a
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2024-06-29 14:08:00 +0200

    Object control fix, documentation

--- Assets/Script/Controller/ObjectControl.cs
+++ Assets/Script/Controller/ObjectControl.cs
@@ -16,9 +16,9 @@ namespace Script.Controller {
         private Vector3 _offset;
         private bool _isHoldingObject;
 
-        [Header("Mouse")]
-        public float holdThreshold = 0.2f; // How long must be button pressed
-        public float moveThreshold = 5f; // If it is less than this, it is considered immobility
+        [Header("Mouse control")]
+        public float holdThreshold = 0.2f; // How long the button must be pressed
+        public float moveThreshold = 5f; // How much the mouse has to move
         private float _mouseDownTime;
         private Vector3 _mouseDownPosition;
         private bool _isHoldingMouse;
@@ -39,34 +39,25 @@ namespace Script.Controller {
                 _isHoldingMouse = false;
                 _isMovingMouse = false;
                 SetDistance();
-
-                var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out var hit)) {
-                    if (hit.transform == _selectedObject) {
-                        _isHoldingObject = true;
-                    }
-                    else {
-                        _isHoldingObject = false;
-                    }
-                }
+                SetHoldingObject();
             }
 
             if (Input.GetMouseButton(0)) {
-                if (Vector3.Distance(_mouseDownPosition, Input.mousePosition) > moveThreshold) {
+                if (IsMovingMouse()) {
                     _isMovingMouse = true;
                     MoveObject();
                 }
                 else {
-                    if (!_isHoldingMouse && Time.time - _mouseDownTime >= holdThreshold) {
+                    if (IsHoldingMouse()) {
                         _isHoldingMouse = true;
-                        Select();
+                        SelectObject();
                     }
                 }
             }
 
             if (Input.GetMouseButtonUp(0)) {
                 if (!_isMovingMouse && !_isHoldingMouse) {
-                    Select();
+                    SelectObject();
                 }
 
                 _isHoldingMouse = false;
@@ -74,7 +65,7 @@ namespace Script.Controller {
             }
         }
 
-        private void Select() {
+        private void SelectObject() {
             var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out var hit)) {
                 if (!hit.collider.CompareTag(Tags.Selectable)) return;
@@ -107,14 +98,49 @@ namespace Script.Controller {
             }
         }
 
+        /// <summary>
+        /// Set distance from main camera to object.
+        /// Set offset from object middle to mouse position.
+        /// </summary>
         private void SetDistance() {
             if (_selectedObject is null) return;
             var position = _selectedObject.position;
-            _offset = position - _mainCamera.ScreenToWorldPoint(
-                new Vector3(Input.mousePosition.x, Input.mousePosition.y,
-                    _mainCamera.WorldToScreenPoint(position).z));
+            var screenPoint = _mainCamera.WorldToScreenPoint(position);
+            _distance = screenPoint.z;
+            _offset = position -
+                      _mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+                          _distance));
+        }
 
-            _distance = Vector3.Distance(_mainCamera.transform.position, position);
+        /// <summary>
+        /// Checks if the selected object has been hit.
+        /// </summary>
+        private void SetHoldingObject() {
+            var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit)) {
+                if (hit.transform == _selectedObject) {
+                    _isHoldingObject = true;
+                }
+                else {
+                    _isHoldingObject = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check that the mouse button has been pressed for longer than the threshold.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsHoldingMouse() {
+            return !_isHoldingMouse && Time.time - _mouseDownTime >= holdThreshold;
+        }
+
+        /// <summary>
+        /// Check if the mouse has been moved.
+        /// </summary>
+        /// <returns></returns>
+        private bool IsMovingMouse() {
+            return Vector3.Distance(_mouseDownPosition, Input.mousePosition) > moveThreshold;
         }
 
         private void Outline(Transform selectedObject, bool enabled) {
